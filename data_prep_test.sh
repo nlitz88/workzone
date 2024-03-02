4a8647973b5aae70217c55d8e8ca916886972f87
Nathan Litzinger <nlitz88@gmail.com>
2024-03-02 11:41:20 -0500

    Updated data_pre_test.sh all in one script

--- data_prep_test.sh
+++ data_prep_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # run python setup
-python setup.py develop &&
-
-# fetch pretrained models
-./tools/download_pretrained.sh &&
+./bevfusion_setup.sh &&
 
 # run data preparation
-python tools/create_data.py nuscenes --root-path data/nuscenes --out-dir data/nuscenes --extra-tag nuscenes --version v1.0-mini &&
+./bevfusion_data_prep.sh &&
 
 # test model using visualize.py
-torchpack dist-run -np 8 python tools/visualize.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml
+./bevfusion_visualize.sh
+
+# UPDATE: Broke this script out into multiple, separate scripts so that data
+# prep and setup doesn't have to run every single time. You'll need to run the
+# first two scripts called here if it's your first time running in the
+# container.
