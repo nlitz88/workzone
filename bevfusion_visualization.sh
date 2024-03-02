a2101de45ddb1558a92ea2fe674a7e846d9d696d
Nathan Litzinger <nlitz88@gmail.com>
2024-03-02 12:19:01 -0500

    Updated visualization run script to visualize gt annotations on train split.

--- bevfusion_visualization.sh
+++ bevfusion_visualization.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # test model using visualize.py
-torchpack dist-run -np 8 python tools/visualize.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml
+torchpack dist-run -np 8 python tools/visualize.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml --split "train"
