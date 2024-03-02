d7b2b4f5188ce945f5f82626fe549a2eafbf1638
Nathan Litzinger <nlitz88@gmail.com>
2024-03-02 12:01:59 -0500

    Added commands to fix ownership.

--- bevfusion_setup.sh
+++ bevfusion_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Change ownership of conda installation. Might be a faster way of doing this,
+# but this works for now. Should really do this (if at all) when building the
+# Docker image.
+sudo chown -R $(id -u -n):$(id -g -n) /opt/conda/lib/python3.8/
+sudo chown -R $(id -u -n):$(id -g -n) ./mmdet3d.egg-info/
+
 # run python setup
 python setup.py develop &&
 
